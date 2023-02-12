4482fd2ca0445fbebe2d7ea242115531cd0b4703
Bergmark <jobe2617@ros.ling.su.se>
2023-02-12 21:10:46 +0100

    .

--- 1.0/trainer.py
+++ 1.0/trainer.py
@@ -52,13 +52,13 @@ class trainer(object):
         model.to(self.device)
 
 
-        optimizer = AdamW(model.parameters(), lr=5e-5)
+        #optimizer = AdamW(model.parameters(), lr=5e-5)
         #optimizer = torch.optim.AdamW(model.parameters)
 
         num_training_steps = self.epochs * len(train_dataloader)
         lr_scheduler = get_scheduler(
                 "linear",
-                optimizer=optimizer,
+                #optimizer=optimizer,
                 num_warmup_steps=0,
                 num_training_steps=num_training_steps
                 )
