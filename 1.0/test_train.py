8ab4761de1d19c0d6f94df5f4dcc55f62e6d3f21
Bergmark <jobe2617@ros.ling.su.se>
2023-02-12 15:04:56 +0100

    .

--- 1.0/test_train.py
+++ 1.0/test_train.py
@@ -43,7 +43,7 @@ def train_folds(set_no, folds=[0,1,2,3,4], loadpath="../data/datasets/",
             print("Dataset created and loaded!")
         
         test_data = dataset["test"]
-        model_trainer = trainer(dataset, filename=(filename + ".model"),
+        model_trainer = trainer(dataset, (filename + ".model"),
                 epochs=epochs, checkpoint="../models/essay_model.model")
         new_predictions = model_trainer.train()
         predictions = predicitons + new_predictions
