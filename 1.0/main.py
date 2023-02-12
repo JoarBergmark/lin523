f92b893d350281262e3a2e237bb98372fb9fb5f1
JoarBergmark <jobe2617@ros.ling.su.se>
2023-02-12 22:48:19 +0100

    .

--- 1.0/main.py
+++ 1.0/main.py
@@ -73,7 +73,10 @@ def train_folds(set_no, folds=[0,1,2,3,4], loadpath="../data/datasets/",
 
         #def tokenize_function(example):
         #    return tokenizer(essay["text"], truncation=True)
-        print(predictor(KeyDataset(dataset["test"], "text"))) 
+        for out in predictor(KeyDataset["test"], "text"), batch_size=8,
+            truncation=True):
+            print(out)
+
         expected_scores = [out["label"][6:] for out in predictor(
             KeyDataset(dataset["test"], "text"), batch_size=8, truncation=True)]
         true_scores = [int(label) for label in dataset["test"]["labels"]]
